ed33049910ae0780d1c3f01010aab41dcec33ca1
joaovsdo <joaotitoschris@gmail.com>
2025-06-16 11:34:29 -0300

    Add: js, event listener

---
+++ index.js
@@ -0,0 +1,6 @@
+const cookieButton = document.querySelector('.js-cookie-button');
+
+
+cookieButton.addEventListener('click', function() {
+    document.querySelector('.cookie-container').classList.add("accepted");
+})
